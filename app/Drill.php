3c1c138374d28cc308724b0695088fa3c00c4ccc
MichitakaInoue <michirug11@i.softbank.jp>
2019-06-25 23:28:13 +0900

    DBにインサート

---
+++ app/Drill.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//このクラス名の複数系をテーブル名として、自動的に判別してSQLを作って操作する  Drillsをテーブル名とする
+class Drill extends Model //このModelクラスにDBを操作するメソッドが入っている 
+{
+    //DBで間違ったとしても変更させたくないカラム(ユーザーiD 、権限など)にはロックをかけておくことができる
+        //同じユーザーでも、そこの権限が違うとかそういうカラムを作っている場合がある勝手に変更されると困るので、ロックをかけること
+    //ロックのかけ方にはfillableもしくはgurdedの２つがある
+    //どちらかしか指定はできず、
+    //モデルがその属性以外を持たなくなる　
+
+    //fillableの場合は変更したいカラムをどんどん書いていく
+     //これらは変更していくべきところなので
+     //DB操作で修正画面から更新させるなどで、fillメソッドを使うと更新が簡単 -->fillableを使うこと
+    protected  $fillable = ['title', 'category_name', 'problem0', 'problem1', 'problem2', 'problem3', 'problem4',
+    'problem5', 'problem6', 'problem7', 'problem8', 'problem9'];
+
+    //これで、テーブル名はDrills　カラムを設定
+    //このmodelを使って、DBの操作を行って行くのだがロックを
+
+
+    //ここは変更されたくないもの テーブルなので運用していく中で考える
+    // protected $gurded = ['id'];
+}
--- app/Http/Controllers/DrillsController.php
+++ app/Http/Controllers/DrillsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Drill;  //名前空間を指定する
 
 class DrillsController extends Controller
 {
@@ -17,24 +17,72 @@ class DrillsController extends Controller
 
     public function create(Request $request)
     { //requiredは必須項目 |で区切ること  
+      //string フィールドはタイプnullであることを許容する場合はそのフィールドでnullableを指定
 
         $request->validate([
             'title' => 'required|string|max:255',
             'category_name' => 'required|string|max:255',
             'problem0' => 'required|string|max:255',
-            'problem1' => 'required|string|max:255',
-            'problem2' => 'required|string|max:255',
-            'problem3' => 'required|string|max:255',
-            'problem4' => 'required|string|max:255',
-            'problem5' => 'required|string|max:255',
-            'problem6' => 'required|string|max:255',
-            'problem7' => 'required|string|max:255',
-            'problem8' => 'required|string|max:255',
-            'problem9' => 'required|string|max:255',
+            'problem1' => 'string|nullable|max:255',
+            'problem2' => 'string|nullable|max:255',
+            'problem3' => 'string|nullable|max:255',
+            'problem4' => 'string|nullable|max:255',
+            'problem5' => 'string|nullable|max:255',
+            'problem6' => 'string|nullable|max:255',
+            'problem7' => 'string|nullable|max:255',
+            'problem8' => 'string|nullable|max:255',
+            'problem9' => 'string|nullable|max:255',
         ]);
+
+
+        //実際にDBに保存 モデルを呼び出す　
+        //DrillモデルはModelクラスを拡張するもので、DB操作のためのメソッドが入っている
+        $drill = new Drill;
+
+
+
+        //1つ１つ入れるか
+        // $drill->title = $request->title;
+        // $drill->category_name = $request->category_name;
+        // $drill->save();
+
+
+
+
+        //fillを使って一気に入れるか
+        //$fillabeleを使っていないと変なデータが入り込んだ場合に勝手にDBが更新されてしまうので注意すること
+
+        //Drillモデルにはfillメソッドが入っている
+        //fillの引数として、更新したいものを全部入れること allメソッドを使う
+        //それを元にsaveメソッドを呼ぶことでDBが更新される  レコードに手を加えるときに使うもの 
+        //saveを呼ぶだけで、更新なのか挿入なのか削除なのか自動で判断してSQLを作ってDBに問い合わせる
+
+        //例えば、レコードがすでに入っていた場合には、更新と判断する。
+        //しかし、本来ならば更新されたくない値も入ってしまうことになるので、modelで定義した$fillableで変更したいものだけ指定すれば
+        //$fillableで指定されていないものは更新されない
+
+
+
+        //ここでSQLのエラーがあった場合には  try catchを自動的にやってくれる
+        //エラーメッセージを個別に表示させる処理も書くことができる
+        $drill->fill($request->all())->save();
+
+
+
+
+        //登録の処理が終わったのでリダイレクト
+        //withメソッドを使うことでsessionに値を詰めることができる
+        //第１session変数の名前   第２引数にメッセージを入れる　　アンスコ２つで
+        //このメッセージはビューに渡す(当たり前)
+        return redirect('/drills/new')->with('flash_message', __('Registerd'));
+
+
+    
+
     }
 }
 
 
 
-//Drillsコントローラー
+
+//これはDrillsコントローラーである
