f8d5bedc0b4a54a3d12ce35c7bf1494b38559f75
MichitakaInoue <michirug11@i.softbank.jp>
2019-06-27 14:59:32 +0900

    vueの連携まで

--- resources/views/drills/show.blade.php
+++ resources/views/drills/show.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 
+
+
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
+                    {{-- ここら辺で表示しているものはフロント側でいじりたいのでphp側のデータをフロント側に渡す必要がある --}}
                     <div class="card-header">
                         {{__('Practice').'['.$drill->title.']'}}
                     </div>
@@ -12,6 +16,19 @@
                         <p>{{$drill->problem0}}</p>
                         <p>{{$drill->problem1}}</p>
                         <p>{{$drill->problem2}}</p>
+
+                        {{-- ここでVueを使っている --}}
+                        <div id="app">
+                            {{-- デフォルトだとこの中ではvue.jsが有効 --}}
+                            {{-- example-componentはlaravelに入っているサンプルのコンポーネント --}}
+                            {{-- laravel(php)の変数をvue(js)に渡す --}}
+                            <example-component>
+                                {{-- この中はbladeテンプレ(php)の書き方になる --}}
+                                title="{{__('Practice').'['.$drill->title.']'}}"
+                                {{-- 変数名の前に:をつけると全部が配列形式で渡すことができる --}}
+                                :drill="{{$drill}}"
+                            </example-component>
+                        </div>
                     </div>
                 </div>
             </div>
